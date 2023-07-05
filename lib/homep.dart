649cfcf5a83245ce986166e001cca5f13458f2d7
feeeerr <martinssssz19@gmail.com>
2023-07-04 23:28:35 -0300

    tela1

---
+++ lib/homep.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 10,
+                  ),
+                  child: SvgPicture.asset(
+                    'assets/images/estabelecimento.svg',
+                    height: 120,
+                    width: 120,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Nome do App',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 190),
+                  child: Text(
+                    'Welcome to NOME DO APP',
+                    style: TextStyle(fontSize: 19,fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  left: 70,
+                  right: 70,
+                  bottom: 10,
+                ),
+                child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('clicou');
+                  },
+                  child: Text('PESSOA'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                    minimumSize: MaterialStateProperty.all<Size>(Size(200, 50)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('clicou');
+                  },
+                  child: Text('ESTABELECIMENTO'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                    minimumSize: MaterialStateProperty.all(Size(200, 50)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100.0),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,6 @@
-vksbdghbsfbv
+import 'package:flutter/material.dart';
+import 'package:front_end/homep.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner:false, home: MyApp(),));
+}
